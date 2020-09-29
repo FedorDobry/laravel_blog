5ba1d879774f8cd98a8a32c3818373a3c085e3c9
FedorDobry <fedya-dobrynin1@mail.ru>
2020-09-29 19:41:48 +0300

    Sec

---
+++ src/database/seeders/BlogCategoriesTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class BlogCategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = [];
+
+        $cName = 'Без категории';
+        $categories[] = [
+            'title' => $cName,
+            Str::of($cName)->slug(),
+            'parent_id' => 0,
+        ];
+    }
+}
